8763421c801ed9f27018f1106af13600cf2c7009
Jens Ulferts <j.ulferts@finn.de>
2018-05-29 08:35:46 +0200

    wip - adapt wp create service to claim attachments

--- app/services/work_packages/set_attributes_service.rb
+++ app/services/work_packages/set_attributes_service.rb
@@ -62,9 +62,6 @@ class WorkPackages::SetAttributesService
     if attributes.key?(:attachment_ids)
       work_package.attachments_replacements = Attachment.where(id: attributes[:attachment_ids])
     end
-    #attachment_attributes = attributes.slice(:attachment_ids)
-    #primary_attributes = attributes.except(:attachment_ids)
-    #work_package.attributes = primary_attributes
     work_package.attributes = attributes.except(:attachment_ids)
 
     set_default_attributes
